05676a9a6cada8191cf9d94f56f765f094920169
RozenkovaH <swisschizz@inbox.ru>
2024-01-08 00:25:47 +0500

    example test

---
+++ app/src/androidTest/java/com/example/myawesomeapp/MyAwesomeTests.kt
@@ -0,0 +1,40 @@
+package com.example.myawesomeapp
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.core.AllOf.allOf
+import org.junit.After
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MyAwesomeTests {
+    private lateinit var scenario: ActivityScenario<MainActivity>
+
+    @Before
+    fun setup() {
+        scenario = ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun checkMainScreen() {
+        onView(
+            allOf(
+                withId(R.id.text_home),
+                withText("This is home Fragment")
+            )
+        ).check(matches(isDisplayed()))
+    }
+
+    @After
+    fun tearDown() {
+        scenario.close()
+    }
+}
